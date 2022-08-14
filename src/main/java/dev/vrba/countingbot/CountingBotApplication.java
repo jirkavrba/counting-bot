1bcb11608ea13b48f6009613d22e994980425894
Jiří Vrba <jiri@vrba.dev>
2022-08-14 11:36:45 +0200

    Initial commit

---
+++ src/main/java/dev/vrba/countingbot/CountingBotApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.countingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountingBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/countingbot/CountingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.countingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
