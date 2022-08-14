f10dc91632adc26d78475c8307b596a49401957c
Jiří Vrba <jiri@vrba.dev>
2022-08-14 14:16:57 +0200

    Add expression parsing and evaluation support

--- src/main/java/dev/vrba/countingbot/discord/listeners/MessageListener.java
+++ src/main/java/dev/vrba/countingbot/discord/listeners/MessageListener.java
@@ -1,5 +1,6 @@
 package dev.vrba.countingbot.discord.listeners;
 
+import com.fathzer.soft.javaluator.DoubleEvaluator;
 import dev.vrba.countingbot.discord.DiscordEventListener;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
@@ -7,6 +8,8 @@ import discord4j.core.object.reaction.ReactionEmoji;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Component
 public class MessageListener implements DiscordEventListener {
@@ -19,8 +22,34 @@ public class MessageListener implements DiscordEventListener {
                 // Message was sent in a guild
                 .filter(event -> event.getGuildId().isPresent())
                 // Replace this with checking for numbers, evaluation expressions and reacting with appropriate emoji
-                .flatMap(event -> event.getMessage().addReaction(ReactionEmoji.unicode("\uD83D\uDDFF")))
+                .flatMap(event ->
+                        this.evaluate(event.getMessage().getContent())
+                                .map(evaluation -> this.processNumber(event, evaluation))
+                                .orElseGet(Mono::empty))
                 .then();
     }
 
+    /**
+     * Evaluate the provided input (or return an empty optional if the input is not valid)
+     *
+     * @param input Input obtained from a
+     * @return Parsed integer after evaluation or empty optiona
+     */
+    private Optional<Integer> evaluate(@NonNull String input) {
+        if (!input.matches("[\\d \\-+/*()]+")) {
+            return Optional.empty();
+        }
+
+        try {
+            return Optional.of(new DoubleEvaluator().evaluate(input).intValue());
+        }
+        // An invalid expression was provided
+        catch (IllegalArgumentException exception) {
+            return Optional.empty();
+        }
+    }
+
+    private Mono<Void> processNumber(@NonNull MessageCreateEvent event, int number) {
+        return event.getMessage().addReaction(ReactionEmoji.unicode("\uD83D\uDDFF"));
+    }
 }
