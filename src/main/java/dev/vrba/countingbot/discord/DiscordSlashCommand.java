605972f15b709fb7cc89075f22df77ebeae78786
Jiří Vrba <jiri@vrba.dev>
2022-08-15 22:23:17 +0200

    Add discord slash command interface definition

--- src/main/java/dev/vrba/countingbot/discord/DiscordSlashCommand.java
+++ src/main/java/dev/vrba/countingbot/discord/DiscordSlashCommand.java
@@ -1,4 +1,16 @@
 package dev.vrba.countingbot.discord;
 
-public class DiscordSlashCommand {
+import discord4j.core.event.domain.command.ApplicationCommandEvent;
+import discord4j.discordjson.json.gateway.ApplicationCommandCreate;
+import org.springframework.lang.NonNull;
+import reactor.core.publisher.Mono;
+
+public interface DiscordSlashCommand {
+
+    @NonNull
+    ApplicationCommandCreate define();
+
+    @NonNull
+    Mono<Void> handle(@NonNull ApplicationCommandEvent event);
+
 }
